73d4387413fcfb370c64538205c330ea75dc0cc7
Стахов Иван <stahov.ia@localhost.localdomain>
2022-11-01 14:05:47 +0300

    v0.1

--- WishListProject/WishList/views.py
+++ WishListProject/WishList/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from django.http import JsonResponse, HttpResponse, FileResponse
+from django.shortcuts import render, redirect
 
-# Create your views here.
+def login_page(request):
+    if request.method == 'GET':
+        # если юзер залогинен - редирект на основную стр
+        if request.user.is_authenticated:
+            return redirect('/')
+        return render(request, 'login.html')
+    if request.method == 'POST':
+        username = request.POST.get('login', '')
+        password = request.POST.get('password', '')
+
+        if username == '' or password == '':
+            #messages.error(request, 'Заполните все поля!')
+            return redirect('/login')
+
+        user = authenticate(username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('/')
+        else:
+            #messages.error(request, 'Неправильный логин или пароль!')
+            return redirect('/login')
+
+
+def logout_page(request):
+    if request.method == 'POST':
+        logout(request)
+    return redirect('/login')
--- WishListProject/WishListProject/urls.py
+++ WishListProject/WishListProject/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from Pass.views import login_page, logout_page
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login', login_page),
+    path('logout', logout_page),
 ]
