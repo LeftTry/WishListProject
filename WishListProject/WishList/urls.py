9a93c6fd828b106bb4c59f9fc1defc09c7281bad
LeftTry <50211739+LeftTry@users.noreply.github.com>
2022-11-15 14:41:50 +0300

    Update urls.py

--- WishListProject/WishList/urls.py
+++ WishListProject/WishList/urls.py
@@ -6,4 +6,5 @@ app_name = 'wishes'
 
 urlpatterns = [
     re_path(r'^delete/(?P<pk>[0-9]+)/$', views.delete, name='delete'),
+    re_path(r'^delete/adm/(?P<pk>[0-9]+)/$', views.delete_adm, name='delete_obj_adm'),
 ]
